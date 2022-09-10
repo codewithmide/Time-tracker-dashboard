74bf7acf4147bf6fb7670b7d7e1cda14e4ed0092
Olumide Micheal <olumidemicheal7@gmail.com>
2022-09-10 14:46:45 +0100

    added functionalities to the daily and weekly tab

--- script.js
+++ script.js
@@ -1,12 +1,100 @@
 const daily = document.getElementById("daily");
-const weekly = document.getElementById("weekly");
-const monthly = document.getElementById("monthly");
 const firstHour = document.getElementById("first-hour");
 const firstPW = document.getElementById("first-prev-week");
+const secondHour = document.getElementById("second-hour");
+const secondPW = document.getElementById("second-prev-week");
+const thirdHour = document.getElementById("third-hour");
+const thirdPW = document.getElementById("third-prev-week");
+const fourthHour = document.getElementById("fourth-hour");
+const fourthPW = document.getElementById("fourth-prev-week");
+const fifthHour = document.getElementById("fifth-hour");
+const fifthPW = document.getElementById("fifth-prev-week");
+const sixthHour = document.getElementById("sixth-hour");
+const sixthPW = document.getElementById("sixth-prev-week");
+
 
 daily.addEventListener("click", () => {
+    daily.style.color = "white";
+
     let firstDaily = "5hrs";
-    let firstPrevWeek = "Last week - 7hrs" 
+    let firstPrevWeek = "Last week - 7hrs";
+
+    let secondDaily = "1hr"
+    let secondPrevWeek = "Last week - 2hrs";
+
+    let thirdDaily = "0hr"
+    let thirdPrevWeek = "Last week - 1hrs";
+
+    let fourthDaily = "1hr"
+    let fourthPrevWeek = "Last week - 1hrs";
+
+    let fifthDaily = "1hr"
+    let fifthPrevWeek = "Last week - 3hrs";
+
+    let sixthDaily = "0hr"
+    let sixthPrevWeek = "Last week - 1hrs";
+
     firstHour.innerHTML = firstDaily;
     firstPW.innerHTML = firstPrevWeek;
+
+    secondHour.innerHTML = secondDaily;
+    secondPW.innerHTML = secondPrevWeek;
+
+    thirdHour.innerHTML = thirdDaily;
+    thirdPW.innerHTML = thirdPrevWeek;
+
+    fourthHour.innerHTML = fourthDaily;
+    fourthPW.innerHTML = fourthPrevWeek;
+
+    fifthHour.innerHTML = fifthDaily;
+    fifthPW.innerHTML = fifthPrevWeek;
+
+    sixthHour.innerHTML = sixthDaily;
+    sixthPW.innerHTML = sixthPrevWeek;
 });
+
+
+const weekly = document.getElementById("weekly");
+
+weekly.addEventListener("click", () => {
+    daily.style.color = "white";
+
+    let firstWeekly = "32hrs";
+    let firstPrevWeek = "Last week - 36hrs";
+
+    let secondWeekly = "10hr"
+    let secondPrevWeek = "Last week - 8hrs";
+
+    let thirdWeekly = "4hr"
+    let thirdPrevWeek = "Last week - 7hrs";
+
+    let fourthWeekly = "4hr"
+    let fourthPrevWeek = "Last week - 5hrs";
+
+    let fifthWeekly = "5hr"
+    let fifthPrevWeek = "Last week - 10hrs";
+
+    let sixthWeekly = "2hr"
+    let sixthPrevWeek = "Last week - 2hrs";
+
+    firstHour.innerHTML = firstWeekly;
+    firstPW.innerHTML = firstPrevWeek;
+
+    secondHour.innerHTML = secondWeekly;
+    secondPW.innerHTML = secondPrevWeek;
+
+    thirdHour.innerHTML = thirdWeekly;
+    thirdPW.innerHTML = thirdPrevWeek;
+
+    fourthHour.innerHTML = fourthWeekly;
+    fourthPW.innerHTML = fourthPrevWeek;
+
+    fifthHour.innerHTML = fifthWeekly;
+    fifthPW.innerHTML = fifthPrevWeek;
+
+    sixthHour.innerHTML = sixthWeekly;
+    sixthPW.innerHTML = sixthPrevWeek;
+});
+
+
+const monthly = document.getElementById("monthly");
